e444ad874c81f922bf6e5efbb57b2a3f301be930
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2022-12-20 13:59:13 +0100

    Iteración#3

---
+++ script3.js
@@ -0,0 +1,47 @@
+// Iteración #3 //
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+];
+
+let sonidosRepetidos = {};
+
+for(user of users) {
+    for(sound in user.favoritesSounds) {
+        if(!sonidosRepetidos[sound]) {
+            sonidosRepetidos[sound] = 1;
+        }
+        else {
+            sonidosRepetidos[sound] += 1;
+        }
+    }
+}
+
+console.log(sonidosRepetidos);
